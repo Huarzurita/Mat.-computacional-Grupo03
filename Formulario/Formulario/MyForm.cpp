69686056d5ce71d89775579f849fb465bd8aa39e
Huarzurita <miguelx_1@hotmail.com>
2022-09-20 21:11:44 -0500

    Implementacion Matriz en Interfaz
    Solo implemente el código que puso Nori en la carpeta pero no en el formulario xd

--- Formulario/Formulario/MyForm.cpp
+++ Formulario/Formulario/MyForm.cpp
@@ -1,7 +1,11 @@
+
 #include "MyForm.h"
-using namespace Formulario;
+using namespace System;
+using namespace Windows::Forms;
+using namespace MateComputacional;
 int main() 
 {
-    Application::Run(gcnew MyForm);
+	Application::EnableVisualStyles();
+	Application::Run(gcnew MateComputacional::MatrizVisual);
 	return 0;
 }
